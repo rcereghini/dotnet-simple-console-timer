42106fc751b4f3bfd119bae2cff8dba64c39adf7
Robert Cereghini <rcereghini@gmail.com>
2020-01-29 22:24:12 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Timers;
+
+namespace myApp
+{
+    class Program
+    {
+        static int ticks = 1;
+        private static Timer theTimer;
+
+        static void Main(string[] args)
+        {
+            theTimer = new System.Timers.Timer();
+            theTimer.Interval = 1000;
+            theTimer.Elapsed += onTimerTick;
+
+            theTimer.AutoReset = true;
+            theTimer.Enabled = true;
+            
+            Console.WriteLine("Press the Enter key to exit the program at any time... ");
+            Console.ReadLine();
+        }
+
+    private static void onTimerTick(Object source, System.Timers.ElapsedEventArgs e){
+        Console.WriteLine("The time is now: " + DateTime.Now);
+        Console.WriteLine(ticks + " seconds have passed.");
+        ticks++;
+    }
+    }
+}
